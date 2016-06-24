e095c26056d84ef148669004859dd946add2f574
Diego Lapiduz <diego.lapiduz@gsa.gov>
2016-06-23 20:36:50 -0500

    Loop every hour and run a goroutine per page

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -9,6 +12,13 @@ import (
 )
 
 func main() {
+	for {
+		Start()
+		time.Sleep(time.Hour * 1)
+	}
+}
+
+func Start() {
 
 	// Connect to S3
 	svc := s3.New(session.New(), &aws.Config{Region: aws.String("us-east-1")})
@@ -23,10 +33,11 @@ func main() {
 	}
 
 	count := 0
+	var wg sync.WaitGroup
 	// List objects
 	for _, b := range resp.Buckets {
 		lastKey := ""
-		perPage := int64(10000)
+		perPage := int64(1000)
 		for {
 			params := &s3.ListObjectsInput{
 				Bucket:  b.Name,
@@ -41,15 +52,21 @@ func main() {
 			}
 
 			// Is each object encrypted?
-			for _, o := range objects.Contents {
-				go func(bucket *string, key *string) {
-					if !CheckEncrypted(svc, bucket, key) {
-						EncryptObject(svc, bucket, key)
-						fmt.Println(*key)
+			wg.Add(1)
+			go func(bucket s3.Bucket, obj []*s3.Object) {
+				defer wg.Done()
+				for _, o := range obj {
+					if strings.HasSuffix(*o.Key, "/") || strings.HasPrefix(*o.Key, ".") {
+						continue
+					}
+					if !CheckEncrypted(svc, bucket.Name, o.Key) {
+						EncryptObject(svc, bucket.Name, o.Key)
+						fmt.Println(*o.Key)
 						count++
 					}
-				}(b.Name, o.Key)
-			}
+				}
+
+			}(*b, objects.Contents)
 
 			// Paginate
 			if *objects.IsTruncated {
@@ -61,7 +78,8 @@ func main() {
 		}
 
 	}
-
+	fmt.Println("Waiting")
+	wg.Wait()
 	fmt.Println("Encrypted", count, "objects")
 }
 
